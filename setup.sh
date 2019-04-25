1dcb4b6ac70be8a4dfbc769b4dd273e52dfc9aae
Sven Dowideit <SvenDowideit@home.org.au>
2019-04-25 12:50:35 +1000

    add the user to the adm group - needed for debian
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
+sudo apt-get dist-upgrade
 sudo apt-get install -yq curl
 
 curl -o adafruit-trinket.rules https://raw.githubusercontent.com/FollyEngine/circuitplayground/master/adafruit-trinket.rules
@@ -10,3 +11,5 @@ sudo cp adafruit-trinket.rules /etc/udev/rules.d/
 # Or if that doesn't work try:
 sudo udevadm control --reload-rules
 sudo udevadm trigger
+
+sudo adduser $USER adm
